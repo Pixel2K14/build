NamelessRom/android_build

Build: Add support for specifying build variant in brunch/breakfast
This little modification allows specifying build variant in
brunch/breakfast commands.
For example we can use "brunch i9300 user" to build user variant
instead of default userdebug.

When no extra argument is given, userdebug is default.

Change-Id: I2d438209f8a14949c64baa66485c811da384d20b

envsetup.sh
@@ -495,6 +495,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     CUSTOM_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
@@ -515,7 +516,10 @@ function breakfast()
             lunch $target
         else
             # This is probably just the custom model name
-            lunch nameless_$target-userdebug
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch nameless_$target-$variant
         fi
     fi
     return $?

Pixel2K14/build

Conflicts:
	envsetup.sh

envsetup.sh
@@ -508,6 +508,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     PIXEL_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
@@ -527,8 +528,11 @@ function breakfast()
             # A buildtype was specified, assume a full device name
             lunch $target
         else
-            # This is probably just the CM model name
-            lunch pixel_$target-userdebug
+            # This is probably just the custom model name
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch pixel_$target-$variant
         fi
     fi
     return $?